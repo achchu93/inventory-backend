c96dc575e6c4abde6dbe559a97af1a149d9623f2
Ahamed Arshad <achchu.zats@gmail.com>
2021-11-23 11:59:15 +0530

    fixed: location update

--- InventoryApp/serializers/location.py
+++ InventoryApp/serializers/location.py
@@ -5,17 +5,21 @@ from InventoryApp.models.user import User
 class LocationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Location
-		fields = ['id', 'name', 'email', 'address', 'phone', 'is_active', 'users']
+		fields = ['id', 'name', 'email', 'address', 'phone', 'is_active', 'users', 'hours']
 		extra_kwargs = {
 			'address': { 'required': True },
 			'users': { 'required': False, 'read_only': False, 'many': True, 'queryset': User.objects.all() }
 		}
 
 	def update(self, instance, validated_data):
-		if 'users' in validated_data:
-			instance.users.clear()
-			users = validated_data.get('users', [])
-			for user in users:
-				instance.users.add(user)
-			instance.save()
+		for key, value in validated_data.items():
+			if(key == 'users'):
+				instance.users.clear()
+				users = validated_data.get('users', [])
+				for user in users:
+					instance.users.add(user)
+			else:
+				setattr(instance, key, value)
+
+		instance.save()
 		return instance
