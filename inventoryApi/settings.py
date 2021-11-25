0fef98c10577fd6f19b00408730cc63371eabf39
Ahamed Arshad <achchu.zats@gmail.com>
2021-11-26 00:18:48 +0530

    added: whitenose

--- inventoryApi/settings.py
+++ inventoryApi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -50,6 +51,9 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
+
+	'whitenoise.middleware.WhiteNoiseMiddleware',
+
 	# 'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,6 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.joinI(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
