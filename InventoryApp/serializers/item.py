0e7d5a998d52371871ffe8828e9eaa9173ed08ce
Ahamed Arshad <achchu.zats@gmail.com>
2021-12-08 14:32:40 +0530

    fixed: category serializations

--- InventoryApp/serializers/item.py
+++ InventoryApp/serializers/item.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 
-from InventoryApp.models import Item
+from InventoryApp.models import Item, ItemCategory
 from .item_category import ItemCategorySerializer
 
 class ItemSerializer(serializers.ModelSerializer):
-	category = ItemCategorySerializer()
 	class Meta:
 		model = Item
 		fields = ['id', 'name', 'quantity', 'last_bought_price', 'order_by', 'file_name', 'status', 'category']
 		extra_kwargs = {
 			'category': { 'required': True, 'read_only': False }
 		}
+
+	def to_representation(self, instance):
+		data = super().to_representation(instance)
+
+		if data.get('category'):
+			data['category'] = ItemCategorySerializer(ItemCategory.objects.get(id=data.get('category'))).data
+
+		return data
